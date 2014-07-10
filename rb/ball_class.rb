533330390a99596a180738b4c4cbd3f6a6e24737
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-10 14:14:13 -0400

    added update method and made some instance variables accessible

--- rb/ball_class.rb
+++ rb/ball_class.rb
@@ -1,6 +1,10 @@
 require_remote "rb/global_constants.rb"
 
 class Ball
+  attr_accessor :x
+  attr_accessor :y
+  attr_accessor :horizontal_velocity
+  attr_accessor :vertical_velocity
   
   def initialize
     @x = GameConstants::HORIZONTAL_CENTER_OF_FIELD
@@ -14,5 +18,10 @@ class Ball
     GameConstants::CONTEXT.beginPath
     GameConstants::CONTEXT.arc(@x, @y, GameConstants::BALL_RADIUS, GameConstants::BALL_START_ANGLE, GameConstants::BALL_END_ANGLE)
     GameConstants::CONTEXT.fill
+  end
+  
+  def updatePosition
+    @x += @horizontal_velocity / GameConstants.FPS
+    @y += @vertical_velocity / GameConstants.FPS
   end
 end
