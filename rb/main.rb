384aa1b72157b74025162862636b507be4705ffe
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-10 10:36:28 -0400

    added some things in main.rb

--- rb/main.rb
+++ rb/main.rb
@@ -1,5 +1,9 @@
-
-require './paddle_class'
-
-`right_paddle = Paddle.new(40, 40)`
-`right_paddle.draw`
+require_remote "rb/paddle_class.rb"
+require_remote "rb/global_constants.rb"
+require_remote "rb/ball_class.rb"
+ball = Ball.new
+right_paddle = Paddle.new(GameConstants::RIGHT_PADDLE_X_POS, GameConstants::RIGHT_PADDLE_Y_POS)
+left_paddle = Paddle.new(GameConstants::LEFT_PADDLE_X_POS, GameConstants::LEFT_PADDLE_Y_POS)
+ball.draw
+right_paddle.draw
+left_paddle.draw
