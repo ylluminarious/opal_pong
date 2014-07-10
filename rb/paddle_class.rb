55d7a3ce54d3ba59da9d973a48ca61adbbf61e99
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-10 14:15:56 -0400

    added update method

--- rb/paddle_class.rb
+++ rb/paddle_class.rb
@@ -5,6 +5,8 @@ class Paddle
     @y_pos = y_pos
     @width = GameConstants::PADDLE_WIDTH
     @height = GameConstants::PADDLE_HEIGHT
+    @velocity = GameConstants::STOPPED
+    @ball = ball
   end
   
   def draw
@@ -12,4 +14,20 @@ class Paddle
     GameConstants::CONTEXT.fillRect(@x_pos, @y_pos, @width, @height)
   end
   
+  # Method to update the paddle's position.
+  def update_position
+    
+    # If the paddle is not stopped, update position.
+    if @ball.horizontal_velocity != GameConstants::STOPPED && @ball.vertical_velocity != GameConstants::STOPPED
+      
+      # Update position.
+      @y_pos += @velocity / GameConstants::FPS
+      
+      # If the paddle hits the top wall or the bottom wall...
+      if @y_pos < GameConstants::TOP_WALL || (@y + @height) > GameConstants::BOTTOM_WALL
+        @velocity = GameConstants::STOPPED
+      end
+      
+    end
+  end
 end
