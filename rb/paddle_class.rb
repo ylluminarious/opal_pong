5de045094a5901548c6dba83938a41b675a0af97
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-15 02:22:30 -0400

    added collision and made some variables accessible

--- rb/paddle_class.rb
+++ rb/paddle_class.rb
@@ -1,5 +1,12 @@
 require_remote "rb/global_constants.rb"
+
 class Paddle
+  
+  attr_accessor :x_pos
+  attr_accessor :y_pos
+  attr_accessor :width
+  attr_accessor :height
+  
   def initialize (x_pos, y_pos, ball)
     @x_pos = x_pos
     @y_pos = y_pos
@@ -19,15 +26,13 @@ class Paddle
     
     # If the paddle is not stopped, update position.
     if @ball.horizontal_velocity != GameConstants::STOPPED && @ball.vertical_velocity != GameConstants::STOPPED
-      
       # Update position.
       @y_pos += @velocity / GameConstants::FPS
       
       # If the paddle hits the top wall or the bottom wall...
-      if @y_pos < GameConstants::TOP_WALL || (@y + @height) > GameConstants::BOTTOM_WALL
+      if @y_pos < GameConstants::TOP_WALL || (@y_pos + @height) > GameConstants::BOTTOM_WALL
         @velocity = GameConstants::STOPPED
       end
-      
     end
   end
 end
