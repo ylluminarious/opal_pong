f91d3f35747c21e87bd470df810000b691261b68
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-15 02:21:42 -0400

    added a game loop and required the new files

--- rb/main.rb
+++ rb/main.rb
@@ -1,14 +1,13 @@
+require_remote "rb/ball_class.rb"
 require_remote "rb/paddle_class.rb"
 require_remote "rb/global_constants.rb"
-require_remote "rb/ball_class.rb"
+require_remote "rb/game_methods.rb"
 
 # Game objects
 ball = Ball.new
 right_paddle = Paddle.new(GameConstants::RIGHT_PADDLE_X_POS, GameConstants::RIGHT_PADDLE_Y_POS, ball)
-left_paddle = Paddle.new(GameConstants::LEFT_PADDLE_X_POS, GameConstants::LEFT_PADDLE_Y_POS)
+left_paddle = Paddle.new(GameConstants::LEFT_PADDLE_X_POS, GameConstants::LEFT_PADDLE_Y_POS, ball)
 
-def game_tick
-  puts "test"
-end
+game_methods = GameMethods.new(ball, right_paddle, left_paddle)
 
-game_interval = $$.setInterval(game_tick, GameConstants::MILLESECONDS / GameConstants::FPS)
+game_interval = $$.setInterval(Proc.new { game_methods.tick }, GameConstants::MILLESECONDS / GameConstants::FPS)
