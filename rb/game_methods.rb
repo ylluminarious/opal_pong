d41b90e405f4813a805557a1bdd1558f1dc40b1c
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-16 17:17:14 -0400

    added things in the update method

--- rb/game_methods.rb
+++ rb/game_methods.rb
@@ -23,7 +23,29 @@ class GameMethods
   def update
     @ball.update_position(@right_paddle, @left_paddle)
     @right_paddle.update_position
-    @left_paddle.update_position
+    if $game_variables[:which_game] == "opening scene"
+      @ball.update_position(@right_paddle, @left_paddle)
+      @right_paddle.AI_update_position
+      @left_paddle.AI_update_position
+    elsif $game_variables[:which_game] == "one player"
+      @left_paddle.AI_update_position
+    elsif $game_variables[:which_game] == "two player"
+      @left_paddle.update_position
+    end
+    
+    if @right_paddle.score == GameConstants::POINTS_TO_WIN || @left_paddle.score == GameConstants::POINTS_TO_WIN
+      $game_variables[:which_game] = "victory scene"
+      @ball.x_pos = GameConstants::HORIZONTAL_CENTER_OF_FIELD
+      @ball.y_pos = GameConstants::VERTICAL_CENTER_OF_FIELD
+      @right_paddle.x_pos = GameConstants::RIGHT_PADDLE_X_POS
+      @left_paddle.y_pos = GameConstants::RIGHT_PADDLE_Y_POS
+      @left_paddle.x_pos = GameConstants::LEFT_PADDLE_X_POS
+      @left_paddle.y_pos = GameConstants::LEFT_PADDLE_Y_POS
+      @ball.horizontal_velocity = GameConstants::STOPPED
+      @ball.vertical_velocity = GameConstants::STOPPED
+      @right_paddle.velocity = GameConstants::STOPPED
+      @left_paddle.velocity = GameConstants::STOPPED
+    end
   end
   
   def write_text
@@ -56,5 +78,8 @@ class GameMethods
     GameConstants::CONTEXT.clearRect(GameConstants::ORIGIN, GameConstants::ORIGIN, GameConstants::RIGHT_WALL, GameConstants::BOTTOM_WALL)
     update
     draw
+    if $game_variables[:which_game] != "opening scene"
+      $game_variables[:color] = "white"
+    end
   end
 end
