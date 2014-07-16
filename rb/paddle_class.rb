26cff0107e1228b711d0ae9d3db4c988938f4b98
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-16 17:17:56 -0400

    added AI and made velocity instance variable accessible

--- rb/paddle_class.rb
+++ rb/paddle_class.rb
@@ -7,6 +7,7 @@ class Paddle
   attr_accessor :y_pos
   attr_accessor :width
   attr_accessor :height
+  attr_accessor :velocity
   attr_accessor :score
   
   def initialize (x_pos, y_pos, ball)
@@ -34,6 +35,44 @@ class Paddle
       # If the paddle hits the top wall or the bottom wall...
       if @y_pos < GameConstants::TOP_WALL || (@y_pos + @height) > GameConstants::BOTTOM_WALL
         @velocity = GameConstants::STOPPED
+      end
+    end
+    
+    def AI_update_position
+      # -------------------- Start of AI code --------------------
+      
+      # AI constants; these can't go into GameConstants because they use instance variables.
+      CENTER_OF_PADDLE = @y_pos + @height / 2
+      BOTTOM_OF_PADDLE = @y_pos + @height
+      
+      # If the ball is not stopped, update position and look through the code for the AI.
+      if @ball.horizontal_velocity != GameConstants::STOPPED && @ball.vertical_velocity != GameConstants::STOPPED
+        # Update position.
+        @y_pos += @velocity / GameConstants::FPS
+        
+        # If the ball's center is above the paddle's center...
+        if @ball.y_pos < CENTER_OF_PADDLE
+          # ... make the paddle go upwards.
+          @velocity = -GameConstants::LEFT_PADDLE_VELOCITY
+        end
+        
+        # If the ball's center is below the paddle's center...
+        if @ball.y_pos > CENTER_OF_PADDLE
+          # ... make the paddle go downwards.
+          @velocity = GameConstants::LEFT_PADDLE_VELOCITY
+        end
+        
+        # If the paddle is touching the top wall and the ball's center is above the paddle's center (so that the paddle will not stay stopped once the ball goes below it)...
+        if @y_pos < GameConstants::TOP_WALL && @ball.y_pos < CENTER_OF_PADDLE
+          # ... stop the paddle.
+          @velocity = GameConstants::STOPPED
+        end
+        
+        # If the paddle is touching the bottom wall and the ball's center is above the paddle's center (again, so that the paddle will not stay stopped once the ball goes above it)...
+        if BOTTOM_OF_PADDLE > GameConstants::BOTTOM_WALL && @ball.y_pos > CENTER_OF_PADDLE
+          # ... stop the paddle.
+          @velocity = GameConstants::STOPPED
+        end
       end
     end
   end
