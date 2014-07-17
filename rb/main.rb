61d5e0ab9c4261d1653150cf01fc119decd6846e
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-17 12:58:42 -0400

    added some comments and score arguments

--- rb/main.rb
+++ rb/main.rb
@@ -6,16 +6,28 @@ require_remote "rb/input.rb"
 
 # Game objects
 ball = Ball.new
-right_paddle = Paddle.new(GameConstants::RIGHT_PADDLE_X_POS, GameConstants::RIGHT_PADDLE_Y_POS, ball)
-left_paddle = Paddle.new(GameConstants::LEFT_PADDLE_X_POS, GameConstants::LEFT_PADDLE_Y_POS, ball)
+
+right_paddle = Paddle.new(GameConstants::RIGHT_PADDLE_X_POS,
+GameConstants::RIGHT_PADDLE_Y_POS,
+GameConstants::RIGHT_HORIZONTAL_SCORE_POS,
+GameConstants::RIGHT_VERTICAL_SCORE_POS,
+ball
+)
+
+left_paddle = Paddle.new(GameConstants::LEFT_PADDLE_X_POS, GameConstants::LEFT_PADDLE_Y_POS,
+GameConstants::LEFT_HORIZONTAL_SCORE_POS,
+GameConstants::LEFT_VERTICAL_SCORE_POS,
+ball
+)
 
 # New instances of GameMethods and Input.
 game_methods = GameMethods.new(ball, right_paddle, left_paddle)
 event = Input.new(ball, right_paddle, left_paddle)
 
+# Interval that will make the game loop.
 game_interval = $$.setInterval(Proc.new { game_methods.tick }, GameConstants::MILLESECONDS / GameConstants::FPS)
 
-# Keyboard events will run methods from the Input class
+# Keyboard events will run methods from the Input class.
 $$.onkeydown = Proc.new do |input_event|
   event.key_down(input_event)
 end
