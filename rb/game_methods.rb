f6d3a2af3c100dd27d14df4a689b1b7bdba2fa40
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-18 13:56:32 -0400

    got the restart button working

--- rb/game_methods.rb
+++ rb/game_methods.rb
@@ -95,6 +95,23 @@ class GameMethods
     pause_button = Element.find("#pause_button")
     pause_button.on(:click) do
       $$.alert("test")
+    # The restart button will set both players' points to 0 and reset all the game objects' positions, as well as their velocities.
+    restart_button = Element.find("#restart_button")
+    restart_button.on(:click) do
+      if $game_variables[:which_game] != "opening scene" && $game_variables[:which_game] != "victory scene"
+        @right_paddle.score = GameConstants::NO_POINTS
+        @left_paddle.score = GameConstants::NO_POINTS
+        @ball.x_pos = GameConstants::HORIZONTAL_CENTER_OF_FIELD
+        @ball.y_pos = GameConstants::VERTICAL_CENTER_OF_FIELD
+        @right_paddle.x_pos = GameConstants::RIGHT_PADDLE_X_POS
+        @right_paddle.y_pos = GameConstants::RIGHT_PADDLE_Y_POS
+        @left_paddle.x_pos = GameConstants::LEFT_PADDLE_X_POS
+        @left_paddle.y_pos = GameConstants::LEFT_PADDLE_Y_POS
+        @ball.horizontal_velocity = GameConstants::STOPPED
+        @ball.vertical_velocity = GameConstants::STOPPED
+        @right_paddle.velocity = GameConstants::STOPPED
+        @left_paddle.velocity = GameConstants::STOPPED
+      end
     end
   end
     
