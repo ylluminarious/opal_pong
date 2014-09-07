508e71a56b1bec229809cb832d17e5d4febf726e
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-09-07 01:35:28 -0400

    whitespace fixes and constants for the halfway line's code edits

--- rb/global_constants.rb
+++ rb/global_constants.rb
@@ -1,35 +1,40 @@
 module GameConstants
     FIELD = $$[:document].getElementById("playing_field")
     CONTEXT = FIELD.getContext("2d")
-  
+    
     # Constants for the game loop
     FPS = 60
     MILLESECONDS = 1000
-  
+    
     # Right paddle constants
     RIGHT_PADDLE_X_POS = 670
     RIGHT_PADDLE_Y_POS = 175
     RIGHT_HORIZONTAL_SCORE_POS = 507.5
     RIGHT_VERTICAL_SCORE_POS = 70
     RIGHT_PADDLE_VELOCITY = 315
-  
+    
     # Left paddle constants
     LEFT_PADDLE_X_POS = 0
     LEFT_PADDLE_Y_POS = 175
     LEFT_HORIZONTAL_SCORE_POS = 160
     LEFT_VERTICAL_SCORE_POS = 70
     LEFT_PADDLE_VELOCITY = 315
-  
+    
     # Width and height of the paddles
     PADDLE_WIDTH = 30
     PADDLE_HEIGHT = 150
-  
+    
+    # The halves to dvide the paddle by (this is used for collision)
+    HALVES = 2
+    
     # Positions and dimensions of the halfway line
-    HALFWAY_LINE_WIDTH = 15
     HALFWAY_LINE_STEPS = FIELD.height / 15
+    HOW_MUCH_TO_STRETCH_STEPS = HALFWAY_LINE_STEPS / 4
+    STEP_WIDTH = 15
+    STEP_HEIGHT = HALFWAY_LINE_STEPS / 2
     HALFWAY_LINE_X_POS = (FIELD.width - 15) / 2
     HALFWAY_LINE_Y_POS = 0
-  
+    
     # Ball constants
     HORIZONTAL_CENTER_OF_FIELD = 350
     VERTICAL_CENTER_OF_FIELD = 250
@@ -37,26 +42,27 @@ module GameConstants
     BALL_START_ANGLE = 0
     BALL_END_ANGLE = 2 * Math::PI
     BALL_VELOCITY = 400
-  
+    
     # Velocity of stopped things is 0
     STOPPED = 0
-  
-    # 10 points to win
+    
+    # Different amounts of points for different things
     POINTS_TO_WIN = 10
+    ONE_POINT = 1
     NO_POINTS = 0
-  
+    
     # Position constants for the game mode instructions
     SINGLE_PLAYER_BUTTON_INSTRUCTIONS_X_POS = 50
     SINGLE_PLAYER_BUTTON_INSTRUCTIONS_Y_POS = 90
     DOUBLE_PLAYER_BUTTON_INSTRUCTIONS_X_POS = 400
     DOUBLE_PLAYER_BUTTON_INSTRUCTIONS_Y_POS = 90
-  
+    
     # Position constants for "Winner!" text
     RIGHT_WINNER_X_POS = 500
     RIGHT_WINNER_Y_POS = 250
     LEFT_WINNER_X_POS = 150
     LEFT_WINNER_Y_POS = 250
-  
+    
     # Constants for the keys' codes
     SPACEBAR_CODE = 32
     UP_ARROW_CODE = 38
@@ -71,14 +77,14 @@ module GameConstants
     ONE_NUMPAD_CODE = 97
     TWO_CODE = 50
     TWO_NUMPAD_CODE = 98
-  
+    
     # Constants for dimensions of the playing field
     ORIGIN = 0
     TOP_WALL = 0
     RIGHT_WALL = FIELD.width
     BOTTOM_WALL = FIELD.height
     LEFT_WALL = 0
-  
+    
     # Font constants
     TEXT_FONT = "15px Courier"
     SCORE_FONT = "50px Courier"
